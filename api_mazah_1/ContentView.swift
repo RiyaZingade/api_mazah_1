76183834ff29da79b5757e2918d36c7823592ede
Riya Zingade <rzingade506@student.fuhsd.org>
2024-08-20 22:44:46 -0700

    Initial Commit

---
+++ api_mazah_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  api_mazah_1
+//
+//  Created by Riya Zingade on 8/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ api_mazah_1/api_mazah_1App.swift
@@ -0,0 +1,17 @@
+//
+//  api_mazah_1App.swift
+//  api_mazah_1
+//
+//  Created by Riya Zingade on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct api_mazah_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
